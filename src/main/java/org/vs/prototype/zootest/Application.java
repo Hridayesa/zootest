b3137830ce14d8f9e730f9cce9f369f37d11a555
Hridayesa <denis.karpoff@gmail.com>
2016-11-23 16:53:04 +0300

    Первый рабочий вариант

--- src/main/java/org/vs/prototype/zootest/Application.java
+++ src/main/java/org/vs/prototype/zootest/Application.java
@@ -1,14 +1,29 @@
 package org.vs.prototype.zootest;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by dekar on 22.11.2016.
  */
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@RestController
 public class Application {
+    @RequestMapping("/aaa")
+    public String home() {
+        return "Hello World";
+    }
+
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(false).run(args);
+//        new SpringApplicationBuilder(Application.class).web(true).run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
